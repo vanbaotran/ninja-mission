b2e94ad3eea06d15288a0619190641652b7a6f8a
Tran Van <vanbaotran201194@gmail.com>
2021-07-14 22:15:27 +0200

    post routes all done

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -4,17 +4,15 @@ const Post = require('../models/Post.model')
 const Application = require('../models/Application.model')
 
 postRoutes.post("/", (req, res, next) => {
-  console.log('HELLOOOOOO')
+
   if (!req.session.currentUser.profileType ==='recruiter'){
     res.status(403).json({message:'You are not allowed to create posts'});
     return;
-  }
+  } 
+  
   if (!req.body.companyLogo){
-    const companyLogo = req.session.currentUser.companyLogo
-    const {description, position, contract, experienceLevel,codeLanguage,location,funFact,remote} = req.body;
-  } else {
-    const {description, position, contract, experienceLevel,codeLanguage,location,funFact,remote, companyLogo} = req.body;
-  }
+    req.body.companyLogo = req.session.currentUser.companyLogo
+  } 
   req.body.recruiterId = req.session.currentUser._id
   Post.create({...req.body})
     .then(newPost=>{
@@ -35,6 +33,13 @@ postRoutes.get("/", (req, res, next) => {
     .then((ret) => res.status(200).json(ret))
     .catch((err) => res.status(500).json({message:'Users not found'}));
 });
+//GET POST BY RECRUITER ID
+postRoutes.get("/recruiter/:recruiterId", (req, res, next) => {
+  Post.find({recruiterId:req.params.recruiterId})
+    .populate('applicationId')
+    .then((ret) => res.status(200).json(ret))
+    .catch((err) => res.status(500).json({message:'Posts not found'}));
+});
 postRoutes.get("/:id", (req, res, next) => {
   Post.findById(req.params.id)
     .then((ret) => {
@@ -53,29 +58,27 @@ postRoutes.put('/:id',(req,res,next)=>{
   }
   Post.findById(req.params.id)
   .then(postFromDB=>{
-    if(postFromDB.recruiterId !== req.session.currentUser._id){
+    if(postFromDB.recruiterId.toString() !== req.session.currentUser._id){
       res.status(403).json({message:'You are not allowed to edit this post 2222'});
     return;
     }
-    postFromDB = {...req.body}
-
-    postFromDB.save()
-    .then(updatedPost =>{
-      console.log(updatedPost)
+    Post.updateOne({_id:req.params.id},{...req.body})
+    .then(updatedPost=>{
       res.status(200).json({updatedPost})
     })
+    .catch(err=>console.log(err))
   })
   .catch(err=>res.status(500).json({message:"Saving changes went wrong"}))
 })
 postRoutes.delete('/:id',(req,res,next)=>{
   if (!req.session.currentUser.profileType ==='recruiter'){
-    res.status(403).json({message:'You are not allowed to edit this post'});
+    res.status(403).json({message:'You are not allowed to delete this post'});
     return;
   }
   Post.findById(req.params.id)
   .then(thePost=>{
-    if(postFromDB.recruiterId !== req.session.currentUser._id){
-      res.status(403).json({message:'You are not allowed to edit this post 2222'});
+    if(thePost.recruiterId.toString() !== req.session.currentUser._id){
+      res.status(403).json({message:'You are not allowed to delete this posttt'});
     return;
     }
     Post.deleteOne({_id:thePost._id},function(err){
