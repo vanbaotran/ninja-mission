d269a11d97778320bdb6dbc90ba4ca236c9dc96c
lotiko <l.despres@netc.fr>
2021-07-17 16:23:01 +0200

    add upload file in react

--- client/src/components/service.js
+++ client/src/components/service.js
@@ -17,3 +17,31 @@ function signup(name, email, password, profileType) {
 }
 export {signup}
  
+
+//// UPLOAD
+const errorHandler = err => {
+  // console.error(err);
+  throw err;
+};
+
+/* Process to do in form to upload file on cloudinary by the api*/
+/*
+handleFileUpload = e => {
+    const uploadData = new FormData();
+    uploadData.append('imageUrl', e.target.files[0]);
+    uploadFile(uploadData)
+      .then(response => {
+        this.setState({ imageUrl: response.secure_url });
+      })
+      .catch(err => {
+        console.log('Error while uploading the file: ', err);
+      });
+*/
+function uploadFile(uploadData) { /// FormData key of the file must be imageUrl and response data key is secure_url
+  return service
+      .post('/upload', uploadData)
+      .then(res => res.data)
+      .catch(errorHandler);
+}
+export { uploadFile };
+////////////////////////////////////////////////
