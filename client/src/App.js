ec6d1bb9250f2fa2fc67b136cc762328dade3de7
Tran Van <vanbaotran201194@gmail.com>
2021-07-19 19:09:23 +0200

    edit profile incoming

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,39 @@ import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import PostForm from './components/PostForm';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
+import EditProfile from './components/EditProfile'
+import {loggedIn} from './components/service';
+import NavBar from './components/NavBar'
 class App extends React.Component {
   state = {loggedInUser:null}
+  fetchUser() {
+    if (this.state.loggedInUser === null){
+      loggedIn()
+        .then(response => {
+          this.setState({loggedInUser: response})
+        })
+        .catch(err => {
+          this.setState({loggedInUser: false}) 
+        })
+    }
+  }
+  componentDidMount() {
+    this.fetchUser()
+  }
   updateLoggedInUser = (userObj) =>{
     this.setState({loggedInUser:userObj})
   }
   render(){
     return (
       <div className="App">
+      <NavBar/>
         <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route path='/login'  render={()=><Login currentUser={this.state.loggedInUser} updateUser={this.updateLoggedInUser}/>}/>
-          <Route path='/signup' render={()=><Signup updateUser={this.updateLoggedInUser}/>} />
-          <Route path='/postForm' component={PostForm}/>
+          <Route path='/signup' render={()=><Signup currentUser={this.state.loggedInUser} updateUser={this.updateLoggedInUser}/>} />
+          <Route path='/editProfile' render={()=><EditProfile currentUser={this.state.loggedInUser} updateUser={this.updateLoggedInUser}/>} />
+          <Route path='/postForm' render={()=><PostForm {...this.props}/>}/>
         </Switch>
       </div> 
     );
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+const NavBar = () =>{
+  return (
+    <div>
+      <Link to='/login'>Log in</Link>
+      <Link to='/signup'>Sign up</Link>
+      <Link to='/postForm'>Create a Job post</Link>
+      <Link to='/editProfile'>Edit my profile</Link>
+
+    </div>
+  )
+}
+export default NavBar
