da654899fbef5fa1c85098ef8dc6ffbe01a87dd1
lotiko <l.despres@netc.fr>
2021-08-05 18:33:14 +0200

    start css chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -16,6 +16,7 @@ class Chat extends React.Component {
 
   async componentDidMount() {
     const roomId = this.props.match.params.id;
+    let prefix_message = this.props.currentUser.profileType === "recruiter" ? "R_" : "C_";
     // fetch data users
     let arrIds = roomId.split("_"); /// "<recruiterId>_<candidateId>_<application>"
     try {
@@ -29,7 +30,6 @@ class Chat extends React.Component {
       this.props.updateRecruiter(recruiter.data);
 
       let room;
-      console.log("===================");
       let messages = await service.get(`/rooms/${roomId}`);
 
       if (!messages.data) {
@@ -39,19 +39,17 @@ class Chat extends React.Component {
         messages = messages.data.messages;
       }
       console.log(messages);
-      // this.setState({messages: messages})
-      //     console.log( "plop1", messages);
       this.setState({
         roomId: roomId,
         recruiterId: arrIds[0],
         candidateId: arrIds[1],
         messages: messages,
       });
-      //     // this.setState({messages: room.data.messages});
+      /// SOCKET
       this.socket.connect();
       this.socket.emit("join-room", room);
       this.socket.on("receiveMessageFromOther", async (message) => {
-        let newMessages = [...this.state.messages, message];
+        let newMessages = [...this.state.messages, `${prefix_message}${message}`];
         let newRoom = await service.patch(`/rooms/${this.state.roomId}`, { messages: newMessages });
         this.setState({ messages: [...newRoom.data.messages] });
       });
@@ -70,6 +68,7 @@ class Chat extends React.Component {
   sendMyMessage = async (e) => {
     this.socket.emit("sendMessage", this.state.message);
   };
+  ownerMessageData = (mess) => {};
   render() {
     return (
       <>
@@ -94,7 +93,30 @@ class Chat extends React.Component {
           </button>
         </div>
         {this.state.err}
-        {this.state.messages && this.state.messages.map((mess, idx) => <p key={idx}>{mess}</p>)}
+        {this.state.messages &&
+          this.state.messages.map((mess, idx) => {
+            let arrMess = mess.split("_");
+            let avatar, name;
+            if(arrMess[0] === "R") {
+              avatar= this.props.currentRecruiter.avatar ;
+              name= this.props.currentRecruiter.name ;
+            } else if (arrMess[0] === "C") {
+              avatar= this.props.currentCandidate.avatar ;
+              name= this.props.currentCandidate.name ;
+            } else {
+              avatar = false;
+              name = false;
+            }
+            return (
+              <div className="flex-row block-mess" key={idx}>
+              <div className="id-chat bg-ligth-grey">
+                {avatar && <img src={avatar} alt="avatar" />}
+                {name && <p>{name} </p>}
+                </div>
+                <h4>{arrMess[1] || mess}</h4>
+              </div>
+            );
+          })}
       </>
     );
   }
