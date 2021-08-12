6af43a86f1f3a89aa6fe8994404285f5cc2860c5
Tran Van <vanbaotran201194@gmail.com>
2021-08-12 18:16:01 +0200

    Revert "fixing screen height"
    This reverts commit bf188d0a76e0d080dbcda274ec54caffe9d7bf7a.

---
+++ client/src/components/MyApplication.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import service from './service'
+class MyOffersList extends React.Component{
+  state ={
+    offerList:[]
+  }
+  goToDetails = (postId) =>{
+    this.props.history.push(`/posts/${postId}`)
+  }
+
+  componentDidMount(){
+    if(this.props.currentUser){
+      service.get(`/posts/recruiter/${this.props.currentUser._id}`)
+      .then(response=>{
+        this.setState({
+          offerList:response.data
+        })
+      })
+      .catch(err=>console.log(err))
+    }  
+  }
+  render(){
+    return (
+      <div className="list"> 
+      <h1>MY OFFERS </h1>
+      <div>
+     {this.state.offerList.map((offer)=>{
+      return (this.props.currentPostId===offer._id && <div onClick={()=>this.goToDetails(offer._id)} style={{backgroundColor:'#F7E194'}} className="list element" key={offer._id}> 
+         <h2>{offer.position}</h2>
+        </div>) ||
+        (<div onClick={()=>this.goToDetails(offer._id)} style={{}} className="list element" key={offer._id}>
+         <h2>{offer.position}</h2>
+        </div>)
+    })}
+    </div>
+    <button onClick={()=>this.props.history.push('/postform/new')} className='btn red'>POST A NEW OFFER</button>
+      </div>
+    )
+  }
+}
+export default MyOffersList;
